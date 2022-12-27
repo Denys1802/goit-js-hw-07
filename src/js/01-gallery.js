53b5e1c513ca1441f0e64eebdc3139d04698bc58
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2022-12-27 23:11:31 +0200

    gallery-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,18 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
 
-const galleryEl = document.querySelector('.gallery');
-console.log(galleryEl);
 
-// {/* <div class="gallery__item">
-//   <a class="gallery__link" href="large-image.jpg">
-//     <img
-//       class="gallery__image"
-//       src="small-image.jpg"
-//       data-source="large-image.jpg"
-//       alt="Image description"
-//     />
-//   </a>
-// </div> */}
+const galleryContainerEl = document.querySelector('.gallery');
+const createGallery = createGalleryItemsMurkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', createGallery);
+
+galleryContainerEl.addEventListener('click', onClickContainerGallery)
+
+
+
+function createGalleryItemsMurkup(array) {
+	return array.map(({preview, original, description}) => {
+		return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+	}).join('');
+} 
+
+function onClickContainerGallery(evt) {
+	evt.preventDefault();
+
+	if (evt.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="1280" heigth="853">
+`)
+	instance.show();
+
+	galleryContainerEl.addEventListener('keydown', evt => {
+		if (evt.code === "Escape") {
+			instance.close();
+	}
+})
+
+	
+}
+
+
+// function visible() {
+// 	const visible = basicLightbox.visible();
+// 	if (visible) {
+// 		document.addEventListener('keydown', evt => {
+// 	console.log(evt.key);
+// 	if (evt.key === "Escape") {
+// 		instance.close()
+// 	}
+//  })
+// 	}
+// }
+
+
+
+
+
+
