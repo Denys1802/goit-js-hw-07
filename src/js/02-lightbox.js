00734b7aaad9cb0fb8520b1f899bfe28bac2a046
DenysZyrianov <78175281+Denys1802@users.noreply.github.com>
2022-12-28 14:11:33 +0200

    gallery-02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector(".gallery");
+const createGallery = createGalleryItemsMurkup(galleryItems);
+
+galleryContainerEl.innerHTML = createGallery;
+
+//galleryContainerEl.addEventListener('click', onClickContainerGallery)
+
+function createGalleryItemsMurkup(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
+
+// function onClickContainerGallery(evt) {
+// 	evt.preventDefault();
+// 	let lightbox = new SimpleLightbox('.gallery a');
+
+// lightbox.open();
+// }
